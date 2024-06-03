0d133755c9c7ecf133f788072c00e35bf373d17d
Shiv-468 <shivmuwal.147@gmail.com>
2024-06-03 11:38:42 +0530

    change svg to img

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,7 +28,6 @@ dropdown.addEventListener('click', toggleDropdown);
 
 
 var modal = document.getElementById('id01');
-
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
